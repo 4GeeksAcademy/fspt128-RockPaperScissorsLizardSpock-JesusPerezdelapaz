e983e4b8277a14b41b6ecd2cbf000b3d259cddc5
Lgsus23 <jesusbernardops23@gmail.com>
2025-11-21 14:08:47 +0000

    ejercicio terminado

---
+++ pruebas.js
@@ -0,0 +1,28 @@
+//Object.values(reglas)[1][1]
+//object.keys(rules)[]
+//object.value(rules){}
+//rules[].includes()//
+
+
+// console.log(rules["rock"]);
+
+
+// comparador("lizard");
+
+// eleccionComputador();
+// comparador("lizard");
+
+// const pruebaDeincludes = () => {
+
+// console.log((Object.values(rules)[0]).includes("rock"));   <------------------//ESTO FUNCIONA para buscar los valores de las reglas
+    
+// }
+// pruebaDeincludes();
+
+
+// eleccionComputador();
+
+// console.log(Object.keys(rules)[0]);
+// console.log(Object.values(rules)[0][1]);
+// comparador();
+
--- src/app.js
+++ src/app.js
@@ -1,11 +1,53 @@
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+
+
+
+const choices = ["rock", "paper", "scissors", "lizard", "spock"]
+const rules = {
+  rock: ['scissors', "lizard"],
+  paper: ["rock", "spock"],
+  scissors: ["paper", "lizard"],
+  lizard: ["spock", "paper"],
+  spock: ["scissors", "rock"]
+}
+
+const eleccionComputador = () => {
+    let eleccionNum = Math.floor(Math.random() * choices.length);
+    let eleccionWord = Object.keys(rules)[eleccionNum];
+    
+    return eleccionWord
+    
 };
+
+const comparador = (eleccionJugador) => {
+    let eleccionWord = eleccionComputador() ;
+    console.log("has escogido: " + eleccionJugador);
+    console.log("La máquina ha escogido: " + eleccionWord);
+    
+    
+    if (eleccionJugador === eleccionWord) console.log("empate");
+    
+    else if (rules[eleccionJugador].includes(eleccionWord)) console.log("ganaste");
+    else console.log("perdiste");
+    
+    
+    
+};
+comparador("paper")
+
+// console.log(rules["paper"]);
+
+// console.log(Object.keys(rules));
+//object.keys(rules)[]
+// console.log(Object.values(rules)[1][0]);
+//rules[].includes()//
