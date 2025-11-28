421c185801cb4d945a168ea67789bf7618a31e26
Lgsus23 <jesusbernardops23@gmail.com>
2025-11-28 09:25:48 +0000

    correccion final

--- src/app.js
+++ src/app.js
@@ -1,14 +1,4 @@
-// import "bootstrap";
-// import "./style.css";
 
-
-// import "./assets/img/rigo-baby.jpg";
-// import "./assets/img/4geeks.ico";
-
-// window.onload = function() {
-//   //write your code here
-//   console.log("Hello Rigo from the console!");
-// };
 
 
 
@@ -23,9 +13,7 @@ const rules = {
 
 const eleccionComputador = () => {
     let eleccionNum = Math.floor(Math.random() * choices.length);
-    let eleccionWord = Object.keys(rules)[eleccionNum];
-    
-    return eleccionWord
+    return  Object.keys(rules)[eleccionNum]; 
     
 };
 
@@ -41,17 +29,10 @@ const comparador = (eleccionJugador) => {
     console.log("La máquina ha escogido: " + eleccionWord);    
     
     if (eleccionJugador === eleccionWord) console.log("empate");
-    
     else if (rules[eleccionJugador].includes(eleccionWord)) console.log("ganaste");
     else console.log("perdiste");   
     
 };
 
-comparador("totuga")
+comparador("paper")
 
-// console.log(rules["paper"]);
-
-// console.log(Object.keys(rules));
-//object.keys(rules)[]
-// console.log(Object.values(rules)[1][0]);
-//rules[].includes()//
